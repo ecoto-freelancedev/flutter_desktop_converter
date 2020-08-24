458e6802efb7d3db6daa773b51fb6a3fc83ca17d
Eduardo <ecoto30@gmail.com>
2020-08-24 09:31:28 -0600

    UI modified and images directory was deleted

--- lib/screens/converter_page.dart
+++ lib/screens/converter_page.dart
@@ -9,6 +9,7 @@ class ConverterPage extends StatefulWidget {
 class _ConverterPageState extends State<ConverterPage> {
   String _value;
   double _result;
+  Size _screenSize;
   TextEditingController _text;
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
@@ -21,204 +22,212 @@ class _ConverterPageState extends State<ConverterPage> {
 
   @override
   Widget build(BuildContext context) {
+    _screenSize = MediaQuery.of(context).size;
     return Scaffold(
       key: _scaffoldKey,
-      body: Column(
-        children: [
-          Stack(
-            children: [
-              Image(
-                height: 145,
-                width: MediaQuery.of(context).size.width,
-                image: AssetImage('images/fondo.png'),
-                fit: BoxFit.fill,
-              ),
-              Positioned(
-                top: 25,
-                left: 20,
-                width: MediaQuery.of(context).size.width,
-                child: Text(
-                  'Convertidor de longitud',
-                  style: TextStyle(
-                    color: Colors.lightBlue[50],
-                    fontFamily: 'Righteous',
-                    fontSize: 40,
-                  ),
-                ),
-              ),
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(15.0, 20.0, 15.0, 8.0),
-            child: Wrap(
-              spacing: 15,
-              alignment: WrapAlignment.center,
-              crossAxisAlignment: WrapCrossAlignment.center,
-              children: [
-                Text(
-                  'Cantidad:',
-                  style: TextStyle(
-                    fontFamily: 'Righteous',
-                    fontWeight: FontWeight.bold,
-                    fontSize: 25,
-                  ),
-                ),
-                Container(
-                  height: 40,
-                  width: MediaQuery.of(context).size.width * 0.5,
-                  decoration: BoxDecoration(
-                    color: Colors.lightBlue[50],
-                    borderRadius: BorderRadius.circular(25),
-                  ),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: TextField(
-                          controller: _text,
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            color: Colors.indigo[900],
-                            fontSize: 25,
-                            fontFamily: 'Righteous',
-                          ),
-                          decoration: InputDecoration(
-                            border: InputBorder.none,
-                          ),
-                          keyboardType: TextInputType.number,
-                        ),
-                      ),
-                      IconButton(
-                        icon: Icon(Icons.clear),
-                        color: Colors.indigo[900],
-                        onPressed: () => _text.clear(),
-                      )
-                    ],
-                  ),
-                ),
-                RaisedButton(
-                  color: Colors.indigo[900],
-                  child: Text(
-                    'Convertir',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontFamily: 'Righteous',
-                      fontSize: 25,
-                    ),
-                  ),
-                  onPressed: _value == '1' || _value == '2' ? _swapTo : null,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(15),
-                      bottomRight: Radius.circular(15),
-                    ),
-                  ),
-                ),
-              ],
+      backgroundColor: Colors.lightBlue[100],
+      appBar: AppBar(
+        backgroundColor: Colors.lightBlue[50],
+        title: Center(
+          child: Text(
+            'Convertidor de longitud',
+            maxLines: 2,
+            style: TextStyle(
+              color: Colors.lightBlue[300],
+              fontSize: 23,
+              fontFamily: 'Righteous',
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(20.0, 8.0, 20.0, 8.0),
-            child: Wrap(
-              spacing: 15,
-              alignment: WrapAlignment.center,
-              crossAxisAlignment: WrapCrossAlignment.center,
-              children: [
-                DropdownButtonHideUnderline(
-                  child: DropdownButton<String>(
-                    hint: Text(
-                      'Medidas',
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(25.0),
+        child: Column(
+          children: [
+            Container(
+              height: _screenSize.height / 3.5,
+              width: _screenSize.width,
+              padding: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                  color: Colors.lightBlue[300],
+                  borderRadius: BorderRadius.all(Radius.circular(25))),
+              child: FittedBox(
+                fit: BoxFit.contain,
+                child: Column(
+                  children: [
+                    SizedBox(height: 20),
+                    FittedBox(
+                      fit: BoxFit.contain,
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              Text(
+                                'Cantidad:',
+                                style: TextStyle(
+                                  color: Colors.lightBlue[50],
+                                  fontFamily: 'Righteous',
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20,
+                                ),
+                              ),
+                              SizedBox(width: 8),
+                              Container(
+                                height: 30,
+                                width: _screenSize.width * 0.48,
+                                decoration: BoxDecoration(
+                                  color: Colors.lightBlue[50],
+                                  borderRadius: BorderRadius.circular(25),
+                                ),
+                                child: TextField(
+                                  controller: _text,
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    color: Colors.indigo[900],
+                                    fontSize: 18,
+                                    fontFamily: 'Righteous',
+                                  ),
+                                  decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                  ),
+                                  keyboardType: TextInputType.number,
+                                  cursorColor: Colors.indigo[900],
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 20),
+                          Row(
+                            children: [
+                              DropdownButtonHideUnderline(
+                                child: DropdownButton<String>(
+                                  isDense: true,
+                                  hint: Text('Medidas'),
+                                  style: TextStyle(
+                                    color: Colors.lightBlue[50],
+                                    fontSize: 20,
+                                    fontFamily: 'Righteous',
+                                  ),
+                                  dropdownColor: Colors.lightBlue[200],
+                                  items: [
+                                    DropdownMenuItem(
+                                      value: '1',
+                                      child: Text('Metros(m)'),
+                                    ),
+                                    DropdownMenuItem(
+                                      value: '2',
+                                      child: Text('Kilometros(Km)'),
+                                    )
+                                  ],
+                                  onChanged: (value) {
+                                    setState(() {
+                                      _value = value;
+                                      _result = 0.0;
+                                    });
+                                  },
+                                  value: _value,
+                                ),
+                              ),
+                              SizedBox(width: 8),
+                              Text(
+                                _value == '1' ? 'Kilometros(Km)' : 'Metros(m)',
+                                style: TextStyle(
+                                  color: Colors.lightBlue[50],
+                                  fontSize: 20,
+                                  fontFamily: 'Righteous',
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 15),
+                          RaisedButton(
+                            color: Colors.indigo[900],
+                            child: Text(
+                              'Convertir',
+                              style: TextStyle(
+                                color: Colors.lightBlue[50],
+                                fontFamily: 'Righteous',
+                                fontSize: 20,
+                              ),
+                            ),
+                            onPressed:
+                                _value == '1' || _value == '2' ? _swapTo : null,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(15),
+                                bottomRight: Radius.circular(15),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 25),
+            Container(
+              padding: const EdgeInsets.all(8),
+              height: _screenSize.height / 7,
+              width: _screenSize.width / 2.2,
+              decoration: BoxDecoration(
+                color: Colors.lightBlue[300],
+                borderRadius: BorderRadius.all(Radius.circular(25)),
+              ),
+              child: FittedBox(
+                fit: BoxFit.contain,
+                child: Column(
+                  children: [
+                    Text(
+                      'Resultado',
                       style: TextStyle(
-                        fontSize: 25,
+                        color: Colors.lightBlue[50],
+                        fontSize: 30,
                         fontFamily: 'Righteous',
                       ),
                     ),
-                    items: [
-                      DropdownMenuItem(
-                        value: '1',
-                        child: Text(
-                          'Metros(m)',
-                          style: TextStyle(
-                            fontSize: 25,
-                            fontFamily: 'Righteous',
+                    Container(
+                      child: Row(
+                        children: [
+                          SizedBox(width: 10),
+                          Text(
+                            _result == null ? '' : _result.toString(),
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.lightBlue[50],
+                              fontSize: 20,
+                              fontFamily: 'Righteous',
+                            ),
                           ),
-                        ),
+                          SizedBox(width: 8),
+                          Text(
+                            _value == '1' ? 'Kilometros(Km)' : 'Metros(m)',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.lightBlue[50],
+                              fontFamily: 'Righteous',
+                              fontSize: 20,
+                            ),
+                          ),
+                          IconButton(
+                            icon: Icon(Icons.content_copy),
+                            color: Colors.indigo[900],
+                            iconSize: 20,
+                            onPressed: () {
+                              _copyToClipboard(_result);
+                            },
+                          )
+                        ],
                       ),
-                      DropdownMenuItem(
-                        value: '2',
-                        child: Text(
-                          'Kilometros(Km)',
-                          style: TextStyle(
-                            fontSize: 25,
-                            fontFamily: 'Righteous',
-                          ),
-                        ),
-                      )
-                    ],
-                    onChanged: (value) {
-                      setState(() {
-                        _value = value;
-                        _result = 0.0;
-                      });
-                    },
-                    value: _value,
-                  ),
+                    ),
+                  ],
                 ),
-                Icon(
-                  Icons.swap_horiz,
-                  size: 35,
-                ),
-                Text(
-                  _value == '1' ? 'Kilometros(Km)' : 'Metros(m)',
-                  style: TextStyle(
-                    fontSize: 25,
-                    fontFamily: 'Righteous',
-                  ),
-                ),
-              ],
+              ),
             ),
-          ),
-          SizedBox(
-            height: 75,
-          ),
-          Container(
-            width: MediaQuery.of(context).size.width * 0.50,
-            color: Colors.lightBlue[50],
-            child: Wrap(
-              spacing: 15,
-              alignment: WrapAlignment.center,
-              crossAxisAlignment: WrapCrossAlignment.center,
-              children: [
-                Text(
-                  'Resultado:',
-                  style: TextStyle(fontFamily: 'Righteous', fontSize: 20),
-                ),
-                Text(
-                  _result == null ? '' : _result.toString(),
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontFamily: 'Righteous',
-                  ),
-                ),
-                Text(
-                  _value == '1' ? 'Kilometros(Km)' : 'Metros(m)',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontFamily: 'Righteous',
-                  ),
-                ),
-                IconButton(
-                  icon: Icon(Icons.content_copy),
-                  color: Colors.indigo[900],
-                  onPressed: () {
-                    _copyToClipboard(_result);
-                  },
-                )
-              ],
-            ),
-          )
-        ],
+            // SizedBox(height: 200),
+          ],
+        ),
       ),
     );
   }
